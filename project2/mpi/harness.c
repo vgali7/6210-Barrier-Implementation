9fe7d4a946df97a5aa3f49d4ccc75a82add5598d
Sidney Wright <swright3743@gmail.com>
2025-03-03 01:21:05 -0500

    mpi harness and sbatch file

--- project2/mpi/harness.c
+++ project2/mpi/harness.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <sys/time.h>
 #include "gtmpi.h"
 
 int main(int argc, char** argv)
 {
-  int num_processes, num_rounds = 5;
+  int num_processes, num_rounds = 100000;
 
   MPI_Init(&argc, &argv);
-  
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
   if (argc < 2){
     fprintf(stderr, "Usage: ./harness [NUM_PROCS]\n");
     exit(EXIT_FAILURE);
@@ -19,8 +23,19 @@ int main(int argc, char** argv)
   gtmpi_init(num_processes);
   
   int k;
+  double total_time = 0;
+  struct timeval start, end;
   for(k = 0; k < num_rounds; k++){
+    gettimeofday(&start, NULL); 
     gtmpi_barrier();
+    gettimeofday(&end, NULL);
+
+    total_time += (end.tv_sec - start.tv_sec) + ((end.tv_usec - start.tv_usec) / 1000000.0);
+  }
+
+  double avg_time = total_time / num_rounds;
+  if (rank == 0) {
+    printf("Avg time: %f\n", avg_time);
   }
 
   gtmpi_finalize();  
